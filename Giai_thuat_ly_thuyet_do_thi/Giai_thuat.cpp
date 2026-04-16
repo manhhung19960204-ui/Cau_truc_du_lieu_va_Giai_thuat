63ab76b294040ac6deb47731fab35072d3de755d
Manh hung <manhhung19960204@gmail.com>
2026-04-16 23:57:36 +0700

    giai thuat bai 10 tinh tong mang

--- Giai_thuat_ly_thuyet_do_thi/Giai_thuat.cpp
+++ Giai_thuat_ly_thuyet_do_thi/Giai_thuat.cpp
@@ -18,6 +18,27 @@ int CheckPrimeNumber(int x) {
 	return true;
 }
 
+int bai10() {
+	// sum mang 2 chieu 
+	int m , n ;
+    cin >> m >> n ;
+    int a[m][n];
+    int sum = 0 ;
+    for(int i=0 ; i< m ; i++) {
+        for(int j=0; j < n ; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    for(int i=0 ; i< m ; i++) {
+        for(int j=0; j < n ; j++) {
+            sum = sum + a[i][j];
+        }
+    }
+
+    cout << sum ;
+}
+
 int bai8(){
 	
 	// kiem tra co phai la day don dieu hay khong ?
